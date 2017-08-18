c50e2cb24555c78c59eaa10f5886f8cadf23af5b
Ali Bassam <alibassam.27@gmail.com>
2017-08-18 11:17:45 +0300

    servlet 4

---
+++ servlet4/src/main/java/servlet/MainServlet.java
@@ -0,0 +1,33 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletMapping;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.MappingMatch;
+
+@WebServlet(urlPatterns={"/", "/main", "*.html", "/main/*"})
+public class MainServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		//Discover the manner in which this HttpServlet was invoked
+		HttpServletMapping httpServletMapping = req.getHttpServletMapping();
+		
+		//Retrieve MappingMatch Enum
+		MappingMatch mappingMatch = httpServletMapping.getMappingMatch();
+		
+		//Print additional results
+		System.out.println("Mapping Match: " + mappingMatch);
+		System.out.println("Match Value: " + httpServletMapping.getMatchValue());
+		System.out.println("Pattern: " + httpServletMapping.getPattern());
+		System.out.println("Servlet Name: " + httpServletMapping.getServletName());
+	}
+}
